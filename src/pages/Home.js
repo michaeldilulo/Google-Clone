2f5d3c148a744b818f488efe0facbf843125fdf6
michael dilulo <snoleopard01001101@gmail.com>
2020-11-23 14:34:20 -0800

    Pages folder, with Home.js file

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__header">
+
+            </div>
+
+            <div className="home__body">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
