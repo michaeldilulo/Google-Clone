ec3da41e316b2bc9a01a5db3dd273463e327cc4e
michael dilulo <snoleopard01001101@gmail.com>
2020-11-23 14:47:27 -0800

    Dependencies add, Links show up on screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Home from './pages/Home'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="app">
+
+      <Router>
+
+        <Switch>
+          <Route path="/search">
+          </Route>
+          <Route path="/">
       <Home />
+          </Route>
+        </Switch>
+
+      </Router>
+
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Home.css'
+import {Link} from "react-router-dom"
 
 function Home() {
     return (
         <div className="home">
             <div className="home__header">
-
+                <div className="home__headerLeft">
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className="home__headerRight">
+                        <Link to='/gmail'>Gmail</Link>
+                        <Link to='/images'>Images</Link>
+                </div>
             </div>
 
             <div className="home__body">
