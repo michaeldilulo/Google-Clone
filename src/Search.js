ecdc5cea807e010b33ca1a167c863b62e0665e7a
michael dilulo <snoleopard01001101@gmail.com>
2020-11-23 15:30:58 -0800

    useState, onChange input, onClick

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Search.css"
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from '@material-ui/core';
 
 function Search() {
+    const [input, setInput] = useState('')
+
+    const search = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className="search">
             <div className="search__input">
                 <SearchIcon className="search__inputIcon" />
-                <input />
+                <input value={ input} onChange={event => setInput(event.target.value)} />
                 <MicIcon />
             </div>
 
             <div className="search__buttons">
-                <Button variant="outlined">Google Search</Button>
+                <Button onClick={ search} variant="outlined">Google Search</Button>
                 <Button variant="outlined">Im Feeling Lucky</Button>
             </div>
 
