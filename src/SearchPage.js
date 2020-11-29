e34ef57df8da1d9f122f3ba702a79db8246d6bf0
michael dilulo <snoleopard01001101@gmail.com>
2020-11-29 10:46:47 -0800

    Links on Page prior to styling

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,6 +5,12 @@ import useGoogleSearch from './useGoogleSearch'
 import Response from './response'
 import { Link } from "react-router-dom"
 import Search from './Search'
+import SearchIcon from '@material-ui/icons/Search'
+import DescriptionIcon from '@material-ui/icons/Description'
+import ImageIcon from '@material-ui/icons/Image'
+import LocalOfferIcon from '@material-ui/icons/LocalOffer'
+import RoomIcon from '@material-ui/icons/Room'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue()
@@ -26,6 +32,44 @@ function SearchPage() {
 
                 <div className="searchPage__headerBody">
                 <Search hideButtons />
+
+                    <div className="searchPage__options">
+                        <div className="searchPage__optionsLeft">
+                            <div className="searchPage__option">
+                                <SearchIcon />
+                                <Link to="/all">All</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <DescriptionIcon />
+                                <Link to="/news">News</Link>
+                            </div>
+                                <div className="searchPage__option">
+                                <ImageIcon />
+                                <Link to="/images">Images</Link>
+                            </div>
+                                <div className="searchPage__option">
+                                <LocalOfferIcon />
+                                <Link to="/shopping">shopping</Link>
+                            </div>
+                                <div className="searchPage__option">
+                                <RoomIcon />
+                                <Link to="/maps">maps</Link>
+                            </div>
+                                <div className="searchPage__option">
+                                <MoreVertIcon />
+                                <Link to="/more">more</Link>
+                            </div>
+                        </div>
+                        <div className="searchPage__optionsRight">
+                            <div className="searchPage__option">
+                                <Link to="/settings">Settings</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <Link to="/tools">Tools</Link>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
